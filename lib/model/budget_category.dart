af9910d8fe4403b049d0888f69a72d3e8be72c64
hafizflow <hafizur.rahman.cs@gmail.com>
2024-08-02 15:07:24 +0600

    Initial commit

---
+++ lib/model/budget_category.dart
@@ -0,0 +1,13 @@
+class BudgetCategory {
+  final String id;
+  final String name;
+  final double budgetedAmount;
+  final double spentAmount;
+
+  BudgetCategory({
+    required this.id,
+    required this.name,
+    required this.budgetedAmount,
+    required this.spentAmount,
+  });
+}
